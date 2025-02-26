97d43c091c281be1bfd2c981493c1baf590c3fa2
Chris <4knowin9@gmail.com>
2025-02-26 15:02:32 -0400

    added initial mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import './App.css'
+import {TimerComponent} from "./components/TimerComponent.tsx";
+import {Button} from "./components/Button.tsx";
+import {FooterBar} from "./components/FooterComponent.tsx";
 
 function App() {
 
   return (
     <>
+      <main className={"flex flex-col gap-24"}>
+        <h1 className={"text-2xl"}>POMODORO</h1>
 
+        <div id={"timerContainer"} className={"flex flex-col justify-center items-center gap-8"}>
+          <TimerComponent></TimerComponent>
+
+          <div className={"flex flex-row justify-center gap-8"}>
+            <Button buttonText={"START"}></Button>
+            <Button buttonText={"STOP"}></Button>
+          </div>
+        </div>
+      </main>
+
+      <FooterBar></FooterBar>
     </>
-  )
+  );
 }
 
 export default App
