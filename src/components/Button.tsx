b39c79069169aa67730f4ad272209a039c2739e6
Chris <4knowin9@gmail.com>
2025-02-26 15:01:28 -0400

    added styling for Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,13 @@ type ButtonProps = {
 
 export function Button({buttonText}: ButtonProps) {
   return (
-    <button className={"p-2 px-4 bg-gray-300 rounded-full"}>
-      <p>{buttonText}</p>
+    <button className={`
+      py-3 px-8 rounded-full 
+      bg-[var(--accent-color)] text-[var(--secondary-bg-color)]
+      hover:brightness-90
+      active:brightness-80
+    `}>
+      <p className={"text-lg"}>{buttonText}</p>
     </button>
   );
 }
