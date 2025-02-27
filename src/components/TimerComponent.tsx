d0279ca03e097403d6a1483efaae94bb6b907f8d
Chris <4knowin9@gmail.com>
2025-02-27 08:56:38 -0400

    added attribute names

--- src/components/TimerComponent.tsx
+++ src/components/TimerComponent.tsx
@@ -22,24 +22,24 @@ export function TimerComponent() {
              animate={{opacity: showSettings ? 0 : 100}}>
           <defs>
             <linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%">
-              <stop offset="5%" stop-color="#878787"></stop>
-              <stop offset="95%" stop-color="#878787"></stop>
+              <stop offset="5%" stopColor="#878787"></stop>
+              <stop offset="95%" stopColor="#878787"></stop>
             </linearGradient>
           </defs>
           <path
             d="M 0,600 L 0,150 C 106.65071770334927,176.3444976076555 213.30143540669854,202.68899521531102 314,180 C 414.69856459330146,157.31100478468898 509.444976076555,85.58851674641147 602,96 C 694.555023923445,106.41148325358853 784.9186602870813,198.9569377990431 877,217 C 969.0813397129187,235.0430622009569 1062.8803827751199,178.58373205741626 1157,155 C 1251.1196172248801,131.41626794258374 1345.55980861244,140.7081339712919 1440,150 L 1440,1200 L 0,1200 Z"
-            stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="0.2"
+            stroke="none" strokeWidth="0" fill="url(#gradient)" fillOpacity="0.2"
             className="transition-all duration-300 ease-in-out delay-150 path-0"></path>
 
           <defs>
             <linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%">
-              <stop offset="5%" stop-color="#878787"></stop>
-              <stop offset="95%" stop-color="#878787"></stop>
+              <stop offset="5%" stopColor="#878787"></stop>
+              <stop offset="95%" stopColor="#878787"></stop>
             </linearGradient>
           </defs>
           <path
             d="M 0,600 L 0,350 C 106.6602870813397,335.0430622009569 213.3205741626794,320.0861244019139 292,302 C 370.6794258373206,283.9138755980861 421.37799043062205,262.6985645933014 524,290 C 626.622009569378,317.3014354066986 781.1674641148325,393.1196172248804 904,410 C 1026.8325358851675,426.8803827751196 1117.952153110048,384.82296650717706 1202,365 C 1286.047846889952,345.17703349282294 1363.023923444976,347.58851674641147 1440,350 L 1440,1200 L 0,1200 Z"
-            stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="0.20"
+            stroke="none" strokeWidth="0" fill="url(#gradient)" fillOpacity="0.20"
             className="transition-all duration-300 ease-in-out delay-150 path-1"></path>
         </motion.svg>
       </motion.div>
