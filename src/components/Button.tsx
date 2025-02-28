971561a0d394b030ef704e5a1cf75f02b202ca13
Chris <4knowin9@gmail.com>
2025-02-28 09:48:45 -0400

    added onClick prop to Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,19 @@
 type ButtonProps = {
   buttonText: string
+  onClickEvent: () => void
 }
 
-export function Button({buttonText}: ButtonProps) {
+export function Button({buttonText, onClickEvent}: ButtonProps) {
   return (
-    <button className={`
-      py-3 px-8 rounded-full 
-      bg-[var(--accent-color)] text-[var(--secondary-bg-color)]
-      hover:brightness-90
-      active:brightness-80
-    `}>
+    <button
+      className={`
+        py-3 px-8 rounded-full 
+        bg-[var(--accent-color)] text-[var(--secondary-bg-color)]
+        hover:brightness-90
+        active:brightness-80
+      `}
+      onClick={(): void => onClickEvent()}
+    >
       <p className={"text-lg"}>{buttonText}</p>
     </button>
   );
