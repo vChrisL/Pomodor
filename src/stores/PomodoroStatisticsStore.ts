1d2630a1b3d08996a1d4780f6a61bbd4673a04e4
Chris <4knowin9@gmail.com>
2025-03-01 11:59:56 -0400

    added localstorage to store total focus and break period counters

--- src/stores/PomodoroStatisticsStore.ts
+++ src/stores/PomodoroStatisticsStore.ts
@@ -16,16 +16,26 @@ type StatisticsStore = {
 export const useStatisticsStore = create<StatisticsStore>(set => ({
   currentFocusPeriods: 0,
   currentBreakPeriods: 0,
-  totalFocusPeriods: 0,
-  totalBreakPeriods: 0,
+  totalFocusPeriods: localStorage.getItem("pomodoroStatistics") ? JSON.parse(localStorage.getItem("pomodoroStatistics")!)[0] : 0,
+  totalBreakPeriods: localStorage.getItem("pomodoroStatistics") ? JSON.parse(localStorage.getItem("pomodoroStatistics")!)[1] : 0,
   incrementPeriod: (isFocusPeriod: boolean): void => {
     if(isFocusPeriod) {
       set(state => ({ currentFocusPeriods: state.currentFocusPeriods + 1 }));
-      set(state => ({ totalFocusPeriods: state.totalFocusPeriods + 1 }));
+      set(state => {
+        const statCount: number[] = [state.totalFocusPeriods + 1, state.totalBreakPeriods];
+        localStorage.setItem("pomodoroStatistics", JSON.stringify(statCount));
+
+        return { totalFocusPeriods: state.totalFocusPeriods + 1 };
+      });
     }
     else {
       set(state => ({ currentBreakPeriods: state.currentBreakPeriods + 1 }));
-      set(state => ({ totalBreakPeriods: state.totalBreakPeriods + 1 }));
+      set(state => {
+        const statCount: number[] = [state.totalFocusPeriods, state.totalBreakPeriods + 1];
+        localStorage.setItem("pomodoroStatistics", JSON.stringify(statCount));
+
+        return { totalBreakPeriods: state.totalBreakPeriods + 1 };
+      });
     }
   }
 }));
