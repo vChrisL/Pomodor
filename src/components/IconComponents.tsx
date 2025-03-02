89b4561f6f5fdd7038cb73060b4c3955caab5197
Chris <4knowin9@gmail.com>
2025-03-02 11:13:59 -0400

    fixed attribute names

--- src/components/IconComponents.tsx
+++ src/components/IconComponents.tsx
@@ -23,9 +23,9 @@ export function SunIcon({svgClass}: IconProps) {
   return (
     <svg className={svgClass} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <g fill="none">
-        <g stroke="inherit" stroke-miterlimit="10" stroke-width="1.5" clip-path="url(#siSunDuotone0)">
+        <g stroke="inherit" strokeMiterlimit="10" strokeWidth="1.5" clipPath="url(#siSunDuotone0)">
           <path d="M12 18a6 6 0 1 0 0-12a6 6 0 0 0 0 12Z"/>
-          <path stroke-linecap="round"
+          <path strokeLinecap="round"
                 d="M3 12H1m22 0h-2m-9 9v2m0-22v2M5.636 18.364l-1.414 1.414M19.778 4.222l-1.414 1.414m-12.728 0L4.222 4.222m15.556 15.556l-1.414-1.414M18 12a6 6 0 1 1-12 0a6 6 0 0 1 12 0Z"/>
         </g>
         <defs>
