7af073453824149ea17a216f6ec47c02ad48857a
Chris <4knowin9@gmail.com>
2025-03-04 13:36:18 -0400

    added trySetTitle method

--- src/classes/TodoItem.ts
+++ src/classes/TodoItem.ts
@@ -2,9 +2,24 @@ export class TodoItem {
   private _isComplete: boolean = false;
   private _title: string = "";
 
+  // Getter setter for isComplete state
   public get getIsComplete():boolean { return this._isComplete; }
   public set isComplete(value: boolean) { this._isComplete = value; }
 
+  // Getter setter for title
   public get getTitle(): string { return this._title; }
-  public set title(value: string) { this._title = value; }
+  private set title(value: string) {
+    this._title = value;
+  }
+
+  /**
+   * Sets the title of a to-do item.
+   * @param title To-do item title.
+   * @returns Returns a boolean value. `true` if title was set successfully, `false` otherwise.
+   */
+  public trySetTitle(title: string): boolean {
+    if(title.trim() === "") return false;
+    this.title = title.trim();
+    return true;
+  }
 }
