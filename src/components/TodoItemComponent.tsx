2bd3833f59a12ada4f6dc2b8b9981da22307e5b6
Chris <4knowin9@gmail.com>
2025-03-04 14:12:48 -0400

    added strikethrough when item is marked as complete

--- src/components/TodoItemComponent.tsx
+++ src/components/TodoItemComponent.tsx
@@ -1,18 +1,24 @@
 import {TodoItem as TodoItemType} from "../classes/TodoItem.ts";
+import {useState} from "react";
 
 type TodoItemProps = {
   item: TodoItemType
 }
 
 export function TodoItem({item}: TodoItemProps) {
+  const [isChecked, setIsChecked] = useState<boolean>(item.getIsComplete);
+
   return (
-    <div className={"flex flex-row gap-2 items-start"}>
+    <div className={"flex flex-row gap-2 p-1 items-start border-t border-gray-400"}>
       <input
         className={"mt-2"}
         type={"checkbox"}
         defaultChecked={false}
+        onChange={(e) => {
+          setIsChecked(e.target.checked);
+        }}
       />
-      <p className={"text-lg text-left"}>{item.getTitle}</p>
+      <p className={`text-lg text-left ${isChecked ? 'line-through' : ''}`}>{item.getTitle}</p>
     </div>
   )
 }
