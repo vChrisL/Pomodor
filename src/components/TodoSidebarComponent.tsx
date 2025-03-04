ee4c9ac9fc3aa746d8fee74fa9cee9184d7997da
Chris <4knowin9@gmail.com>
2025-03-04 14:48:24 -0400

    added textarea prompt when creating a new item

--- src/components/TodoSidebarComponent.tsx
+++ src/components/TodoSidebarComponent.tsx
@@ -1,6 +1,7 @@
 import {TodoItem} from "./TodoItemComponent.tsx";
 import {ThemeButton} from "./ToggleThemeComponent.tsx";
 import {useTodoListStore} from "../stores/TodoListStore.ts";
+import {useState} from "react";
 
 
 export function TodoSidebar() {
@@ -8,6 +9,7 @@ export function TodoSidebar() {
   const todoItems = useTodoListStore(state => state.todoItems);
   const addTodoItem = useTodoListStore(state => state.addItem);
 
+  const [isCreatingItem, setIsCreatingItem] = useState<boolean>(false);
 
   return (
     <aside className={`
@@ -17,24 +19,62 @@ export function TodoSidebar() {
         sm:flex sm:p-4
     `}>
       <div>
-        <h1 className={"text-2xl pb-4"}>TODO</h1>
+        <h1 className={"text-2xl pb-4"}>TO-DO</h1>
         <div className={"flex flex-col gap-1"}>
           {
             todoItems.map((item) =>
               <TodoItem key={item.getKey} item={item}/>
             )
           }
-          <button
-            onClick={(): void => {
-              console.log("add item")
-            }}
-            className={`
-                w-full p-2 bg-[var(--accent-color)] rounded-lg
-                hover:brightness-90
-                active:brightness-80
-              `}>
-            Add an item
-          </button>
+          {isCreatingItem && (
+            <div className={"flex flex-col gap-1 p-2"}>
+              <textarea
+                className={"w-full p-2 rounded-lg bg-[var(--primary-bg-color)]"}
+                placeholder={"Add an item"}
+              />
+              <div className={"flex flex-row gap-2"}>
+                <button
+                  onClick={() => {
+                    console.log("create new item")
+                  }}
+                  className={`
+                    w-fit py-1 px-2 bg-[var(--accent-color)] rounded-lg
+                    hover:brightness-90
+                    active:brightness-80
+                  `}
+                >
+                  Add
+                </button>
+                <button
+                  onClick={(): void => {
+                    setIsCreatingItem(false);
+                  }}
+                  className={`
+                    w-fit py-1 px-2 bg-[var(--accent-color)] rounded-lg
+                    hover:brightness-90
+                    active:brightness-80
+                  `}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          )}
+
+          {!isCreatingItem && (
+            <button
+              onClick={(): void => {
+                setIsCreatingItem(true);
+              }}
+              className={`
+                  w-full p-2 bg-[var(--accent-color)] rounded-lg
+                  hover:brightness-90
+                  active:brightness-80
+              `}
+            >
+              Add an item
+            </button>
+          )}
         </div>
       </div>
       <ThemeButton/>
