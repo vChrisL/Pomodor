93ceceb1ae1e285696c9fff699a85ff704b92769
Chris <4knowin9@gmail.com>
2025-03-04 15:07:20 -0400

    added TodoItem hover effect

--- src/components/TodoItemComponent.tsx
+++ src/components/TodoItemComponent.tsx
@@ -9,7 +9,13 @@ export function TodoItem({item}: TodoItemProps) {
   const [isChecked, setIsChecked] = useState<boolean>(item.getIsComplete);
 
   return (
-    <div className={"flex flex-row gap-2 p-1 items-start border-t border-gray-400"}>
+    <>
+    <div
+      className={`
+        flex flex-row gap-2 p-1 items-start rounded-lg
+        hover:bg-[var(--primary-bg-color)]
+      `}
+    >
       <input
         className={"mt-2"}
         type={"checkbox"}
@@ -20,5 +26,7 @@ export function TodoItem({item}: TodoItemProps) {
       />
       <p className={`text-lg text-left ${isChecked ? 'line-through' : ''}`}>{item.getTitle}</p>
     </div>
+    <hr className={"text-[var(--primary-bg-color)] brightness-90"}/>
+    </>
   )
 }
