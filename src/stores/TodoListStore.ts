2b5cd323aa03120ef151a904fad2dc945a8649e0
Chris <4knowin9@gmail.com>
2025-03-04 14:30:54 -0400

    added placeholder todo test data

--- src/stores/TodoListStore.ts
+++ src/stores/TodoListStore.ts
@@ -9,7 +9,13 @@ type TodoListStore = {
 }
 
 export const useTodoListStore = create<TodoListStore>(set => ({
-  todoItems: [],
+  todoItems: [
+    new TodoItem("Implement adding todo items"),
+    new TodoItem("Add ability to change todo item name"),
+    new TodoItem("Add ability to delete todo items"),
+    new TodoItem("Add strike-through when todo item is marked as complete (DONE)"),
+    new TodoItem("Add horizontal break line after each todo item (DONE)"),
+  ],
   addItem: (item: TodoItem): void => {
     set(state => {
       return {todoItems: [...state.todoItems, item]}
