499e097b6109e75857a60662fe92a719dc0075bf
Chris <4knowin9@gmail.com>
2025-03-07 11:21:01 -0400

    added todo list mobile menu

--- src/components/TodoMenuComponent.tsx
+++ src/components/TodoMenuComponent.tsx
@@ -1,7 +1,94 @@
+import {TodoItem} from "./TodoItemComponent.tsx";
+import {useRef, useState} from "react";
+import {useTodoListStore} from "../stores/TodoListStore.ts";
+import {useOnClickOutside} from "../util/onClickOutside.tsx";
+import {TodoItem as TodoItemType} from "../classes/TodoItem.ts";
+
 export function MobileTodoMenu() {
+  const [todoItemTitle, setTodoItemTitle] = useState<string>("");
+  const todoItems = useTodoListStore(state => state.todoItems);
+  const addTodoItem = useTodoListStore(state => state.addItem);
+
+  const [isCreatingItem, setIsCreatingItem] = useState<boolean>(false);
+
+  const formRef = useRef(null);
+  useOnClickOutside(formRef, (): void => {
+    setIsCreatingItem(false)
+  });
+
+  function handleAddItem(): void {
+    try {
+      const item: TodoItemType = new TodoItemType(todoItemTitle);
+      addTodoItem(item);
+      setIsCreatingItem(false);
+    } catch {
+      return;
+    }
+  }
+
   return (
-    <div className={"w-full h-[90%] bg-[var(--primary-bg-color) z-10"}>
-      <h1 className={"text-2xl"}>TODO</h1>
+    <div className={"w-full h-full p-4 z-10 bg-[var(--primary-bg-color)] dark:bg-[var(--dark-primary-bg-color)] dark:text-[var(--dark-text-color)]"}>
+      <div className={"h-full overflow-y-auto"}>
+        <h1 className={"text-2xl pb-4"}>TO-DO</h1>
+        <div className={"flex flex-col gap-1"}>
+          {
+            todoItems.map((item) =>
+              <TodoItem key={item.getKey} item={item}/>
+            )
+          }
+          {isCreatingItem && (
+            <form
+              ref={formRef}
+              className={"flex flex-col gap-1 p-2"}
+            >
+              <textarea
+                className={"w-full p-2 rounded-lg bg-[var(--primary-bg-color)] dark:bg-[var(--dark-primary-bg-color)]"}
+                autoFocus={true}
+                placeholder={"Add an item"}
+                onChange={(e): void => setTodoItemTitle(e.target.value)}
+              />
+              <div className={"flex flex-row gap-2"}>
+                <button
+                  onClick={(e): void => {
+                    e.preventDefault();
+                    handleAddItem();
+                  }}
+                  className={`
+                    w-fit py-1 px-2 bg-[var(--accent-color)] rounded-lg
+                    hover:brightness-90
+                    active:brightness-80
+                  `}
+                >
+                  Add
+                </button>
+                <button
+                  onClick={(): void => setIsCreatingItem(false)}
+                  className={`
+                    w-fit py-1 px-2 bg-[var(--accent-color)] rounded-lg
+                    hover:brightness-90
+                    active:brightness-80
+                  `}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          )}
+
+          {!isCreatingItem && (
+            <button
+              onClick={(): void => setIsCreatingItem(true)}
+              className={`
+                  w-full p-2 bg-[var(--accent-color)] rounded-lg
+                  hover:brightness-90
+                  active:brightness-80
+              `}
+            >
+              Add an item
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
