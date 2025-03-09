5bfacd41513be8ab7077b0de43fd1a4d8ddde471
Chris <4knowin9@gmail.com>
2025-03-09 10:36:48 -0300

    fixed missing constructor argument

--- src/components/TodoSidebarComponent.tsx
+++ src/components/TodoSidebarComponent.tsx
@@ -24,7 +24,7 @@ export function TodoSidebar() {
    */
   function handleAddItem(): void {
     try {
-      const item: TodoItemType = new TodoItemType(todoItemTitle);
+      const item: TodoItemType = new TodoItemType(todoItemTitle, false);
       addTodoItem(item);
       setIsCreatingItem(false);
     } catch {
